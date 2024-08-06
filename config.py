c511715a67c2aeb8e89c964cc2dcccd2e7b91c8e
Love83600 <lover141007@gmail.com>
2024-08-06 22:04:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7082798823:AAFpNrDpcHLLGLjyFd5zZit67Xb29p6GZrs")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("ParmXbot")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("7177710068")
